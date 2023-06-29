5b9535b2ff5a1579ef75adddfbc18200666c7e74
Buki03 <bukihussien@gmail.com>
2023-06-29 22:03:43 +0000

    Added Test File
    Co-authored-by: Celeste Carrasco <celestencarrasco@gmail.com>

--- test_finder.py
+++ test_finder.py
@@ -3,19 +3,15 @@ from finder import users
 
 
 class TestFinder(unittest.TestCase):
-    
+
    # mock_input.side_effect = ['John', 'Doe', 'johndoe@gmail.com', '1', '2', '3']
     def test_data(self):
-        self.mock_input_values = ['John', 'Doe', 'johndoe@gmail.com', '1', '2', '3']
+        self.mock_input_values = [
+            'John', 'Doe', 'johndoe@gmail.com', '1', '2', '3']
         self.assertEqual(users['first_name'][-1], 'John')
         self.assertEqual(users['last_name'][-1], 'Doe')
         self.assertEqual(users['email'][-1], 'johndoe@gmail.com')
-        self.assertEqual(users['city'][-1], 'Ashburn')  
+        self.assertEqual(users['city'][-1], 'Ashburn')
         self.assertEqual(users['interest1'][-1], 'Sports')
         self.assertEqual(users['interest2'][-1], 'Food')
         self.assertEqual(users['interest3'][-1], 'Entertainment')
-    
-    
-
-    
-        
